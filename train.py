dffa6a1301d70e34d9f025b4f93f0ea45e45bbf2
Jose Geraldo Fernandes macaverde604@gmail.com <macaverde604@gmail.com>
2024-07-08 11:59:34 +0000

    add normal class experiment

--- train.py
+++ train.py
@@ -4,16 +4,24 @@ import torch
 import torch.nn as nn
 
 from models.baseline import ResnetBaseline
-from runners.multiclass import Runner
 
-model_label = 'code15'
+# model_label = 'code15'
+# epochs = 15
+# from runners.multiclass import Runner
+# from dataloaders.code15 import CODE as DS
+# from dataloaders.code15 import CODEsplit as DSsplit
+# model = ResnetBaseline(n_classes = 6)
+
+model_label = 'code15normal'
 epochs = 15
+
 from dataloaders.code15 import CODE as DS
 from dataloaders.code15 import CODEsplit as DSsplit
+from runners.normal import Runner
+model = ResnetBaseline(n_classes = 1)
 
 # # init
 database = DS()
-model = ResnetBaseline(n_classes = 6)
 # model = torch.load('output/{}/partial.pt'.format(model_label))
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
